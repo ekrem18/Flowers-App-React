3366a691536dc373c0f39ce59618566826ee91fa
Ekrem <ekrem@gmail.com>
2023-07-10 22:31:45 +0300

    player card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Container } from "react-bootstrap";
-import Header from "./components/Header"
-import ContainerCard from "./components/ContainerCard"
+import Header from "./components/Header";
+import ContainerCard from "./components/ContainerCard";
 
 function App() {
   return (
     <Container className="text-center mt-5">
       <Header />
       <ContainerCard />
-      
+
     </Container>
   );
 }
--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -4,24 +4,39 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {data} from "../helpers/data";
+import PlayerCard from './PlayerCard';
 
 const ContainerCard = () => {
-  const [search, setSearch]=useState("")
-  
-  const handleChange= (e)=>{
-    setSearch(e.target.value)
-  }
-  const filteredData= data.filter(item=> item.name.toLowerCase().includes(search.trim().toLowerCase()))
-  
-  
+    const [search,setSearch] = useState("")
+    console.log(data)
+    const handleChange = (e) =>{
+        console.log(e.target.value)
+        setSearch(e.target.value) //! setter mtodları asenkron olarak çalışır.
+        // console.log(search)
+    }
+    console.log(search);
+
+    const filteredData = data.filter(item=> item.name.toLowerCase().includes(search.trim().toLowerCase()))
+
+    console.log("filter",filteredData)
+
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." onChange={handleChange}/>
-      <Container className='p-3 rounded-4 card-container my-3'>
-        <Row xs={2} md={4} lg={6} className='justify-content-center g-3 '>
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
+      <Container className="p-3 rounded-4 card-container my-3">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
           <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>3 of 3</Col>
+        
+          {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              {...player}
+            />
+          ))}
         </Row>
       </Container>
     </>
